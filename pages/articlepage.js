f25c8c309fe86cb1d085a01b3d52a20c9d6daa7e
Chris Lam <christopherwlam@gmail.com>
2019-01-16 18:20:43 -0800

    Some updates merged from Master and subcommittee positions

--- pages/articlepage.js
+++ pages/articlepage.js
@@ -1,6 +1,7 @@
-import React, { Component} from 'react';
+import React, { Component } from 'react';
 import Link from 'next/link';
 import MainFactory from '../layout/main.js';
+import Pagination from 'react-js-pagination';
 
 
 class ArticlePage extends Component{
@@ -23,57 +24,53 @@ class ArticlePage extends Component{
 
 class ArticleArea extends Component{
 	constructor(props){
-		super(props)
+		super(props);
 		this.state = {
 			articles: [],
-			currentPage: 1
-		}
-		this.add = this.add.bind(this)
-		this.eachLink = this.eachLink.bind(this)
+			currentPage: 1,
+			total: 0
+		};
+		this.eachLink = this.eachLink.bind(this);
+		this.onPageChange = this.onPageChange.bind(this);
 	}
 
 	componentWillMount(){
-		fetch("/api/articles/page/" + this.state.currentPage).then(response => response.json())
-			.then(json => json.forEach(post => this.add(post)));
+		fetch("http://uclacki.org/api/articles/count").then(response => response.json())
+		.then(json => this.setState({total: json}));
+		fetch("http://uclacki.org/api/articles/page/" + this.state.currentPage).then(response => response.json())
+			.then(json => this.setState({articles: json}));
 	}
 
-	add(text) {
-		this.setState(prevState => ({
-			articles: [
-				...prevState.articles,{
-					text
-				}
-			]
-		}))
+	onPageChange(pageNumber) {
+		fetch("http://uclacki.org/api/articles/page/" + pageNumber).then(response => response.json())
+			.then(json => this.setState({articles: json, 
+				currentPage: pageNumber}));
+		console.log(pageNumber);
 	}
 
 	eachLink(link) {
-		console.log(link.text.title);
+		console.log(link.title);
+		// Why do we need unique keys: https://stackoverflow.com/questions/28329382/understanding-unique-keys-for-array-children-in-react-js/43892905#43892905
 		return (
-			<div className="article-entry">
+			<div className="article-entry" key={link._id}>
 				<div className="event-image">
-				{link.text.content.image &&
-								<img src={link.text.content.image.url} />}
+				{link.content.image &&
+								<img src={link.content.image.url} />}
 				</div>
 				<div className="excerpt">
-					<Link className="link" as={`/article/${link.text.title}`} href={`/article?title=${link.text.title}`}><h2>{link.text.title}</h2></Link>	
-					<p dangerouslySetInnerHTML={{ __html: link.text.content.summary }}></p>
+					<Link as={`/article/${link.slug}`} href={`/article?slug=${link.slug}`}><h2>{link.title}</h2></Link>	
+					<p dangerouslySetInnerHTML={{ __html: link.content.summary }}></p>
 				</div>
 
 				<style jsx>{`
-
 					.article-entry {
 						position: relative;
 						display: flex;
 						flex-flow: row nowrap;
-
 						margin: 20px 0;
-
 						// background: white;
-
 						z-index: 1;
 					}
-
 					@media (max-width: 989px) {
 						.article-entry {
 							flex-flow: column nowrap;
@@ -83,14 +80,12 @@ class ArticleArea extends Component{
 							height: auto;
 						}
 					}
-
 					@media (min-width: 989px) {
 						.event-image {
 							max-width: 25%;
 							height: auto;
 						}
 					}
-
 					/* Page effect */
 					.excerpt::before,
 					.excerpt::after {
@@ -103,39 +98,31 @@ class ArticleArea extends Component{
 					  z-index: -1;
 					  transition: all .3s ease-in-out;
 					}
-
 					.excerpt::before {
 					  left: 10px;
 					  transform: skew(-5deg) rotate(-5deg);
 					}
-
 					.excerpt::after {
 					  right: 10px;
 					  transform: skew(5deg) rotate(5deg);
 					}
-
 					.excerpt:hover::before,
 					.excerpt:hover::after {
 					  box-shadow: 0 2px 14px rgba(0,0,0,.4);
 					}
-
 					.excerpt:hover::before {
 					  left: 5px;
 					}
-
 					.excerpt:hover::after {
 					  right: 5px;
 					}
-
 					.event-image {
 						// margin-right: 20px;
 					}
-
 					.event-image img {
 						max-width: 100%;
 						height: auto;
 					}
-
 					.excerpt {
 						position: relative;
 						padding: 20px;
@@ -145,7 +132,6 @@ class ArticleArea extends Component{
 					  box-shadow: 0 2px 14px rgba(0,0,0,.1);
 						// z-index: 1;
 					}
-
 					.excerpt h2 {
 						margin-top: 0;
 						text-decoration: underline;
@@ -160,11 +146,111 @@ class ArticleArea extends Component{
 	}
 
 	render() {
-		console.log(this.state.articles);
 		return(
 			<div>
 				{this.state.articles.map(this.eachLink)}
-			</div>);
+				<Pagination
+		          activePage={this.state.currentPage}
+		          itemsCountPerPage={10}
+		          totalItemsCount={this.state.total}
+		          onChange={this.onPageChange}
+		        />
+
+		        <style jsx global>{`
+		        	.pagination,
+					.pagination li a {
+					  display: flex;
+					  flex-wrap: wrap;
+					  justify-content: center;
+					  align-items: center;
+					  list-style: none;
+					}
+					.pagination li { background-color: lightseagreen; }
+					.pagination a {
+					  font-weight: 300;
+					  padding-top: 1px;
+					  text-decoration:none;  
+					  border: 1px solid rgba(0,0,0,.25);
+					  border-left-width: 0;
+					  min-width:44px;
+					  min-height:44px;
+					  color: rgba(255,255,255,.85);  
+					  box-shadow: inset 0px 1px 0px 0px rgba(255,255,255,.35);
+					}
+					.pagination li:not([class*="current"]) a:hover { 
+					  background-color: rgba(255,255,255,.2);
+					  border-top-color: rgba(0,0,0,.35);
+					  border-bottom-color: rgba(0,0,0,.5);
+					}
+					.pagination li:not([class*="current"]) a:focus,
+					.pagination li:not([class*="current"]) a:active {;
+					  box-shadow: 0px 0px 10px 1px rgba(0,0,0,.25);
+					  border-left-width:1px;
+					}
+					.pagination li.current a { 
+					  padding-top:.25em;
+					  color: rgba(255,255,255,1);
+					  background-color: rgba(255,255,255,.15);
+					  box-shadow: inset 0px 2px 1px 0px rgba(0,0,0,.25);
+					  cursor: default;
+					  pointer-events: none;
+					}
+					@media only screen and ( max-width: 64.063em ) {  
+					  .pagination li:first-child,
+					  .pagination li:last-child {
+					    /* screen readers only */
+					    position: absolute;
+					    top: -9999px;
+					    left: -9999px;
+					  }
+					  .pagination li:nth-of-type(2) a { border-left-width: 1px; }
+					}
+					@media only screen and ( max-width: 40.063em ) {  
+					  .pagination li {
+					    /* screen readers only */
+					    position: absolute;
+					    top: -9999px;
+					    left: -9999px;
+					  }
+					  .pagination li.current,
+					  .pagination li:first-of-type,
+					  .pagination li:last-of-type,
+					  .pagination li:nth-of-type(2),
+					  .pagination li:nth-last-of-type(2){
+					    position: initial;
+					    top: initial;
+					    left: initial;
+					  }
+					  .pagination li:nth-of-type(2) a { border-left-width: 0; }
+					}
+					@media only screen and ( max-width: 30.063em ) {  
+					  
+					  h1 { font-size: 1.35em !important; }
+					  
+					  .pagination li:first-child,
+					  .pagination li:last-child {
+					    /* screen readers only */
+					    position: absolute;
+					    top: -9999px;
+					    left: -9999px;
+					  }
+					  .pagination li:nth-of-type(2) a { border-left-width: 1px; }
+					}
+					@media only screen and ( max-width: 15.063em ) {  /* For watches? */
+					  
+					  .pagination li { width: 50%;}
+					  
+					  .pagination li.current { 
+					    order: 2;
+					    width: 100%;
+					    border-left-width: 1px;
+					      
+					  }  
+					}
+					}
+		        `}</style>
+   			</div>
+   		);
 	}
 }
 export default MainFactory({headerTitle: 'Articles'})(ArticlePage);
--- routes/index.js
+++ routes/index.js
@@ -15,6 +15,10 @@ var routes = {
 // Setup Route Bindings
 exports = module.exports = nextApp => keystoneApp => {
 
+	/*
+	*** Change throw err to 
+	*/
+
 	keystoneApp.use(cors());
 	keystoneApp.use(mongoSanitize());
 
@@ -34,8 +38,9 @@ exports = module.exports = nextApp => keystoneApp => {
 	keystoneApp.post('/api/events/cancel', routes.data.events.cancel);
 
 	keystoneApp.get('/api/articles/all', routes.data.articles.all);
+	keystoneApp.get('/api/articles/count', routes.data.articles.count);
 	keystoneApp.get('/api/articles/page/:page', routes.data.articles.page);
-	keystoneApp.get('/api/articles/:title', routes.data.articles.title);
+	keystoneApp.get('/api/articles/:slug', routes.data.articles.slug);
 
 	keystoneApp.get('/api/pagedata', (req, res) => {
 		const PageData = keystone.list('PageData');
@@ -86,20 +91,33 @@ exports = module.exports = nextApp => keystoneApp => {
 
 	// Clean URL masking on server-side
 	// CLient side can mask using <Link as={`/article/${props.title}`} href={`/article?title=${props.title}`}>
-	keystoneApp.get('/article/:title', (req, res) => {
+	keystoneApp.get('/article/:slug', (req, res) => {
 		const actualPage="/article";
-		const queryParams= {title: req.params.title};
+		const queryParams= {slug: req.params.slug};
 		nextApp.render(req, res, actualPage, queryParams);
 	});
 	// keystoneApp.use('/articleimages/:name', express.static(__dirname + "../static/articleimages"));
 
 
-
+	// Also more masking, specifically for old links to old website, pull up archived articles
+	keystoneApp.get('/:slug', (req, res, next) => {
+		const Article = keystone.list('Article');
+		Article.model
+				.findOne({state: 'archived', slug: req.params.slug})
+				.count()
+				.exec(function(err, result) {
+					if(err)
+						throw err;
+					if(result)
+						nextApp.render(req, res, "/article", {slug: req.params.slug});
+					else
+						next();
+				})
+	});
 
 	// Next request handler
 	const handle = nextApp.getRequestHandler();
 	keystoneApp.get('*', (req, res) => {
 		return handle(req, res);
 	});
-};
-
+};w

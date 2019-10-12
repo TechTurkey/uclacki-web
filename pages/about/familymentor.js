2f5c070de11faa947edeacd3fcf55e88dbe16177
Chris Lam <christopherwlam@gmail.com>
2019-10-12 14:32:36 -0700

    Committee Meeting slide and CNH Kiwanis mentorship

--- pages/about/familymentor.js
+++ pages/about/familymentor.js
@@ -207,7 +207,7 @@ const FamiliesAndMentors = () => {
 					<h3>CNH Circle K Mentorship Application</h3>
 					<p>
 						Apply to be paired with a Kiwanian or Alumni (outside of UCLA CKI!) who will mentor you through school, life, and more!
-						<a href="https://docs.google.com/forms/d/e/1FAIpQLSdiOcFNhMW_hW2Adu9t9xHGuNTWhNuydGXBlphiv9cxHdtm7w/viewform?fbclid=IwAR1qbv2_bupKrZ6BoIxrYmlMs9XKyQT54Wa28PyPHXSFUtnUZRcXaXR9FjQ">
+						<a href="https://bit.ly/cnhmentee1920">
 						Check out the district application here!</a>
 					</p>
 				</div>
